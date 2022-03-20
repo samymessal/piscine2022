2413fb0bf0a2ada06fe572576dc59c7b5e420c77
Samy Messal <samymessal@192.168.1.14>
2022-03-20 17:18:45 +0100

    till ex04 c5

---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int ft_iterative_power(int nb, int power)
+{
+    int result;
+
+    result = 1;
+    if (power == 0)
+        return (1);
+    else if (power < 0)
+        return (0);
+    while (power != 0)
+    {
+        result *= nb;
+        power--;
+    }
+    return (result);
+}
+/*
+int main()
+{
+    printf("%d", ft_iterative_power(-5, 4));
+}
+*/
+
